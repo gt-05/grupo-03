d7d802701e0b58b395a9fe39429b6ab24c9e69eb
unknown <ALDAIR0894@DIGITALCOLLEGE.LOCAL>
2024-10-15 10:44:24 -0300

    Prototipo de Header adicionado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../styles/header.css'
+import Logo from '../assets/logo.svg'
+import Search from '../assets/search.svg'
 
-export function Header(){
-    return(
+export function Header() {
+    return (
         <React.Fragment>
-            <header>Teste</header>
+            <header className='w-full'>
+                <div className='flex  px-40 py-10 align-center justify-evenly'>
+                    <div id="logo-wrapper" className='flex'>
+                        <img src={Logo} alt="Logo Digital College"className='size-10'/>
+                        <h1 className='font-semibold text-[--primary] text-4xl'>Digital Store</h1>
+                    </div>
+                    <div className='bg-[--light-gray-3] w-80 flex items-center justify-between rounded p-2'>
+                        <span>Tênis</span>
+                        <img src={Search} alt="Busca por produto" className='size-6'/>
+                    </div>
+                </div>
+            </header>
         </React.Fragment>
     )
 }
