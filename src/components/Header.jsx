213e7cb8de55a0b8ad90e2d264bcfd39625f41ed
unknown <ALDAIR0894@DIGITALCOLLEGE.LOCAL>
2024-10-21 09:35:40 -0300

    Estilo base da Header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,17 @@ import Market from '../assets/market.svg'
 export default function Header() {
     return (
         <React.Fragment>
-            <header className='w-full'>
-                <div className='flex  px-40 py-10 align-center justify-between'>
+            <header>
+                <div className=' w-full flex  px-40 py-10 align-center justify-between'>
                     <img src={Logo} alt="Logo Digital College" className='' />
-                    <div className='bg-[--light-gray-3] w-full my-4 mx-16 flex items-center justify-between rounded p-2'>
-                        <span className='text-[--light-gray-2] '>Pesquisar produto...</span>
+                    <div className='bg-[--light-gray-3] w-auto flex items-center justify-between rounded p-2'>
+                        <span className='text-[--light-gray-2] pr-80'>Pesquisar produto...</span>
                         <img src={Search} alt="Busca por produto" className='size-6' />
                     </div>
                     <div id="market-wrapper" className='flex items-center'>
-                        <a href="#" className='text-[--dark-gray-2] underline'>Cadastre-se</a>
+                        <a href="#" className='text-[--dark-gray-2] underline pr-8'>Cadastre-se</a>
                         <button className="btn bg-[--primary] text-[--white] rounded px-8 py-2 font-bold">Entrar</button>
-                        <img src={Market} alt="Carrinho" />
+                        <img src={Market} alt="Carrinho" className='pl-10'/>
                     </div>
                 </div>
             </header>
