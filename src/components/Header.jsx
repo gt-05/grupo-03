9ef57136e06d3b747be6c0c006b8a28a6dac8137
unknown <ALDAIR0894@DIGITALCOLLEGE.LOCAL>
2024-10-21 09:18:39 -0300

    estilo do header atualizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,15 +9,12 @@ export default function Header() {
         <React.Fragment>
             <header className='w-full'>
                 <div className='flex  px-40 py-10 align-center justify-between'>
-                    <div id="logo-wrapper" className='flex mx-8'>
-                        <img src={Logo} alt="Logo Digital College" className='size-10' />
-                        <h1 className='font-semibold text-[--primary] text-4xl'>Digital Store</h1>
-                    </div>
+                    <img src={Logo} alt="Logo Digital College" className='' />
                     <div className='bg-[--light-gray-3] w-full my-4 mx-16 flex items-center justify-between rounded p-2'>
-                        <span>Tênis</span>
+                        <span className='text-[--light-gray-2] '>Pesquisar produto...</span>
                         <img src={Search} alt="Busca por produto" className='size-6' />
                     </div>
-                    <div id="market-wrapper" className='flex items-center justify-between w-80'>
+                    <div id="market-wrapper" className='flex items-center'>
                         <a href="#" className='text-[--dark-gray-2] underline'>Cadastre-se</a>
                         <button className="btn bg-[--primary] text-[--white] rounded px-8 py-2 font-bold">Entrar</button>
                         <img src={Market} alt="Carrinho" />
