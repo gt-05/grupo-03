7c2babc9201dd176c171ef47fca943a45e4a4ffb
Israel Martins <israelmartins92@outlook.com>
2024-11-08 11:27:38 -0300

    Modificando arquivos

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header"
+import Slide from "../components/Slide"
+import Footer from '../components/Footer'
+
+export default function HomePage() {
+    return (
+        <>
+        <Header />
+
+        <Slide />
+        
+        <Footer />
+        </>
+
+    );
+}
