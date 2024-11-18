c945b38ffa694cca1e4b3466140a9becffafaf57
Cupnodes <aldairgomesdecastrosales@gmail.com>
2024-11-17 22:25:00 -0300

    estrutura de pastas do Footer ajustada

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 import Header from "../components/Header"
 import Cards from "../components/ProductListing"
 import Slide from "../components/Slide"
+import Footer from '../components/Footer'
 export default function HomePage() {
     return (
         <>
         <Header />
         <Slide/>
         <Cards/>
-       
+        <Footer/>
         </>
 
     );
