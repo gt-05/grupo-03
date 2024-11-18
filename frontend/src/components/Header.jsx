50497028c39cd9209b02f7bde9efcd009cfe8ee5
Cupnodes <aldairgomesdecastrosales@gmail.com>
2024-11-17 22:40:37 -0300

    layout do header atualizado

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import '../styles/header.css'
 import Logo from '../assets/logo.svg'
 import Search from '../assets/search.svg'
+import Market from '../assets/market.svg'
+
 export default function Header() {
     return (
-        <>            
-        <header className='w-full'>
-                <div className='flex  px-40 py-10 align-center justify-evenly'>
-                    <div id="logo-wrapper" className='flex'>
-                        <img src={Logo} alt="Logo Digital College"className='size-10'/>
-                        <h1 className='font-semibold text-[--primary] text-4xl'>Digital Store</h1>
+        <React.Fragment>
+            <header>
+                <div className=' w-screen flex  px-40 py-10 gap-4'>
+                    <img src={Logo} alt="Logo Digital College"/>
+                    <h1 className='font-semibold text-[--primary]'>Digital Store</h1>
+                    <div className='bg-[--light-gray-3] w-auto flex items-center justify-between rounded p-2'>
+                        <span className='text-[--light-gray-2] pr-80'>Pesquisar produto...</span>
+                        <img src={Search} alt="Busca por produto" className='size-6 text-[--light-gray-3]'/>
                     </div>
-                    <div className='bg-[--light-gray-3] w-80 flex items-center justify-between rounded p-2'>
-                        <span>Tênis</span>
-                        <img src={Search} alt="Busca por produto" className='size-6'/>
+                    <div id="market-wrapper" className='flex items-center'>
+                        <a href="#" className='text-[--dark-gray-2] underline pr-8'>Cadastre-se</a>
+                        <button className="btn bg-[--primary] text-[--white] rounded px-8 py-2 font-bold">Entrar</button>
+                        <img src={Market} alt="Carrinho" className='pl-10'/>
                     </div>
                 </div>
             </header>
-        </>
+        </React.Fragment>
     )
 }
